cc69e16066295fadf0298542c0bdff48c62bbd91
carlos.cardoso <carlos.cardoso.15@gmail.com>
2018-09-14 18:46:16 +0100

    fix: added armroutines to build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ docker build -t gazebo_nvidia -f Dockerfile.gazebo_nvidia .
 docker build -t yarp -f Dockerfile.yarp .
 docker build -t icub -f Dockerfile.icub .
 docker build -t vizzy -f Dockerfile.vizzy .
+docker build -t vizzy_armroutines -f Dockerfile.vizzy_armroutines .
 
